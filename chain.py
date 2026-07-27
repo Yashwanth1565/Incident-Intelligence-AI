907b669e64f12e5d9d1b3eac95ffe3c13f4659a1
Yashwanth Boya <yashwanthboya27@gmail.com>
2026-07-27 17:41:12 +0530

    Update chain.py

--- chain.py
+++ chain.py
@@ -16,7 +16,7 @@ load_dotenv()
 # Initialize Gemini Model
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    google_api_key="AQ.Ab8RN6KC0U5sSLjR28vYBaLNsgb8-02yio3vRqF_81E3mP_hLA",
     temperature=0.2
 )
 
